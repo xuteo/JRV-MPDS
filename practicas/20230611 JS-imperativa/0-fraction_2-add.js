98f49bdc2ec0925d65881728c4a2c2ef6d4c324b
Jesús <42238905+xuteo@users.noreply.github.com>
2023-06-12 17:50:13 +0000

    js-imperativo-01

---
+++ practicas/20230611 JS-imperativa/0-fraction_2-add.js
@@ -0,0 +1,26 @@
+const { Console } = require("../node_modules/console-mpds");
+const console = new Console();
+
+
+`Primera fracción:
+Introduce el numerador de la fracción: 2
+Introduce el denominador de la fracción: 3
+Segunda fracción:
+Introduce el numerador de la fracción: 3
+Introduce el denominador de la fracción: 4
+
+La suma de la fracción 2/3 y la fracción 3/4 es la fracción 17/12`;
+
+console.writeln("Primera fracción/n")
+const numerator = console.readNumber("Introduce el numerador de la fracción: ");
+const denominator = console.readNumber("Introduce el denominador de la fracción: ");
+console.writeln("Segunda fracción/n")
+const secondNumerator = console.readNumber("Introduce el numerador de la fracción: ");
+const secondDenominator = console.readNumber("Introduce el denominador de la fracción: ");
+
+const sumNumerators = numerator * secondDenominator + secondNumerator * denominator;
+const sumDenominators = denominator * secondDenominator;
+
+
+console.writeln(`La suma de la fracción ${numerator}/${denominator} y la fracción ${secondNumerator}/${secondDenominator}\
+ es la fracción ${sumNumerators}/${sumDenominators}`)
